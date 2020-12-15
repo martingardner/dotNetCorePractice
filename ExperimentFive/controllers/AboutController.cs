a4fd51d83f27a226f69265ae24905e0e9e453217
martin gardner II <martingardnerii@gmail.com>
2020-12-15 13:06:55 -0600

    added in several more api calls to ExperimentFive as well as newtonsoft.json Json formatter

--- ExperimentFive/controllers/AboutController.cs
+++ ExperimentFive/controllers/AboutController.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ExperimentFive.Controllers
 {
@@ -7,12 +9,48 @@ namespace ExperimentFive.Controllers
         [Route("About")]
         public IActionResult Index() => View();
 
-        [HttpGet("GetData")]
+        [HttpGet("GetString")]
         public string GetData() => "42";   
 
         [HttpGet("AddAndReturn")]   
         public int AddAndReturn(
             [FromQuery] int number1, int number2
         )  => number1 + number2;
+
+        [HttpGet("GetBoolean")]
+        public bool GetBoolean() => true;
+
+        [HttpGet("GetJsonObject")]
+        public string GetJsonObject()
+        {
+            var cat = new cat();
+
+            cat.color = "black";
+            cat.gender = "male";
+            cat.name = "skitter";
+
+            return JsonConvert.SerializeObject(cat);
+        }
+
+        [HttpGet("GetJsonObjectVariables")]
+        public string GetJsonObjectVariables(
+            [FromQuery] string color, string gender, string name
+        )
+        {
+            Console.WriteLine(color, gender, name);
+            var cat = new cat();
+            cat.color = color;
+            cat.gender = gender;
+            cat.name = name;
+            
+            return JsonConvert.SerializeObject(cat);
+        }
+    }
+
+    public class cat
+    {
+        public string color { get; set; }
+        public string gender { get; set; }
+        public string name { get; set; }
     }
 }
--- ExperimentFive/wwwroot/js/about.js
+++ ExperimentFive/wwwroot/js/about.js
@@ -1,1 +1,23 @@
-alert('hit');
+window.onload = function(){
+    
+    fetch('http://localhost:5000/GetString')
+    .then(response => response.json() )
+    .then(data => console.log('GetString', data));
+
+    fetch('http://localhost:5000/AddAndReturn?number1=4&number2=6')
+    .then(response => response.json() )
+    .then(data => console.log('AddAndReturn', data));
+
+    fetch('http://localhost:5000/GetBoolean')
+    .then(response => response.json())
+    .then(data => console.log('GetBoolean', data));
+    
+    fetch('http://localhost:5000/GetJsonObject')
+    .then(response => response.json())
+    .then(data => console.log('GetJsonObject', data));
+
+    fetch('http://localhost:5000/GetJsonObjectVariables?color=red')
+    .then(response => response.json())
+    .then(data => console.log('GetJsonObjectVariables', data))
+
+}
